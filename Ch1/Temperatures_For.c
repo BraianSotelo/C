6656b1d51a62613b6645d6216a93fa8449964b03
BraianSotelo <78566622+BraianSotelo@users.noreply.github.com>
2021-02-25 09:25:59 -0600

    Update Temperatures_For.c

--- Ch1/Temperatures_For.c
+++ Ch1/Temperatures_For.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-main()
+int main()
 {
     int fahr;
     
     printf("Fahrenheit\t Celsius\n");
-                                    /*to go from 300 to 0 we need to change the valueo of 0 to 300, then change the
-                                    condition and then the opposite sign of the relation matemathical*/
-    for(fahr = 300; fahr >= 300; fahr = fahr - 20)
+                                    /*to go from 300 to 0 we need to change the condition by 300 for 0
+                                    to go from the top to the lowest and then the opposite sign of the
+                                    relation matemathical*/
+    for(fahr = 300; fahr >= 0; fahr = fahr - 20)
         printf("%3d\t\t%6.1f\n", fahr, (5.0/9.0)*(fahr - 32));
 }
